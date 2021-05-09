e2d672bd2c68e71ffaacb7e08e7d6fa23be51433
songyi4669 <songyi4669@gmail.com>
2021-05-10 00:05:51 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
+import Header from './components/header';
+import ItemCard from './components/itemCard';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBnG1HJ2t1RsCIW-56x9Rj3KV333ufSw0Q", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/components/itemCard.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ItemCard extends Component {
+    render() {
+        return (
+            <main className="cardWrap">
+                <ul className="itemBox">
+                    <li>dff</li>
+                    <li>sdfsdf</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </main>
+        );
+    }
+}
+
+export default ItemCard;
