3c4e9d5fcf2b89d0bdf47169a0fd21e391296a71
songyi4669 <songyi4669@gmail.com>
2021-05-12 13:06:16 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
+
   const search = query => {
     youtube
       .search(query)
-      .then(videos=>setVideos(videos));
+      .then(videos => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
 };
 
 
@@ -21,8 +31,21 @@ function App({youtube}) {
   return (
     <>
       <div className={styles.app}>
-        <SearchHeader onSearch={search}/>
-        <VideoList videos={videos} />;
+        <SearchHeader onSearch={search} />
+        <section className={styles.content}>
+          {selectedVideo && (
+            <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+          )}
+          <div className={styles.list}>
+            <VideoList
+              videos={videos}
+              onVideoClick={selectVideo}
+              display={selectedVideo ? 'list' : 'grid'}
+            />
+          </div>
+        </section>
       </div>
     </>
   );
--- src/components/service/youtube.js
+++ src/components/service/youtube.js
@@ -7,22 +7,20 @@ class Youtube{
 };
     }
 
-    mostPopular() {
-        return fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBnG1HJ2t1RsCIW-56x9Rj3KV333ufSw0Q",
+    async mostPopular() {
+        const response = await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBnG1HJ2t1RsCIW-56x9Rj3KV333ufSw0Q",
             this.getRequestOptions
-        )
-      .then(response => response.json())
-      .then(result => (result.items))
+        );
+        const result_1 = await response.json();
+        return (result_1.items);
     }
     
-    search(query) {
-        return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=AIzaSyBnG1HJ2t1RsCIW-56x9Rj3KV333ufSw0Q`,
-             this.getRequestOptions
-        )
-            .then(response => response.json())
-            .then(result =>
-                result.items.map(item => ({ ...item, id: item.id.videoId }))
-            );
+    async search(query) {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=AIzaSyBnG1HJ2t1RsCIW-56x9Rj3KV333ufSw0Q`,
+            this.getRequestOptions
+        );
+        const result_1 = await response.json();
+        return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 }
 
