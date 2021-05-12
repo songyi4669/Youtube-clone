314a3db8d038c68436df7f04bce09854e430618a
songyi4669 <songyi4669@gmail.com>
2021-05-12 15:43:09 +0900

    Axios

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import axios from 'axios';
 import '@fortawesome/fontawesome-free/js/all.js';
 import Youtube from './components/service/youtube';
 
 const httpClient=axios.create({
             baseURL: 'https://youtube.googleapis.com/youtube/v3',
-            params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+            params: { key: process.env.REACT_APP_YOUTUBE_API_KEY }
         });
 
 const youtube = new Youtube(httpClient);
